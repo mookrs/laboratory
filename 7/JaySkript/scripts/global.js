6c1963fc553202e99734e62e8224f0be0ca176ef
mookrs <mookrs@gmail.com>
2015-03-13 01:38:55 +0800

    Fix onmouseover doesn't work porblem. Maybe because of index.html, but I have no idea

--- 7/JaySkript/scripts/global.js
+++ 7/JaySkript/scripts/global.js
@@ -116,21 +116,21 @@ function prepareSlideshow () {
         links[i].onmouseover = function () {
             destination = this.getAttribute('href');
             if (destination.indexOf('index.html') != -1) {
-                moveElement('preview', 0, 0, 5)
+                moveElement('preview', 0, 0, 5);
             }
             if (destination.indexOf('about.html') != -1) {
-                moveElement('preview', -150, 0, 5)
+                moveElement('preview', -150, 0, 5);
             }
             if (destination.indexOf('photos.html') != -1) {
-                moveElement('preview', -300, 0, 5)
+                moveElement('preview', -300, 0, 5);
             }
             if (destination.indexOf('live.html') != -1) {
-                moveElement('preview', -450, 0, 5)
+                moveElement('preview', -450, 0, 5);
             }
             if (destination.indexOf('contact.html') != -1) {
-                moveElement('preview', -600, 0, 5)
+                moveElement('preview', -600, 0, 5);
             }      
-        }
+        };
     }
 }
 
