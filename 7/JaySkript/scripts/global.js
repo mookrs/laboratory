0db2b3b0d8dbcbec9e5f74077aef8aa450056b02
mookrs <mookrs@gmail.com>
2015-03-14 15:03:18 +0800

    Add Contact form

--- 7/JaySkript/scripts/global.js
+++ 7/JaySkript/scripts/global.js
@@ -219,8 +219,90 @@ function prepareGallery() {
     }
 }
 
+// Contact
+
+function focusLabels () {
+    if (!document.getElementsByTagName) return false;
+    var labels = document.getElementsByTagName('label');
+    for (var i = 0; i < labels.length; i++) {
+        if (!labels[i].getAttribute('for')) continue;
+        labels[i].onclick = function () {
+            var id = this.getAttribute('for');
+            if (!document.getElementById(id)) return false;
+            var element = document.getElementById(id);
+            element.focus();
+        }
+    }
+}
+
+function resetFields (whichForm) {
+    if (Modernizr.input.placeholder) return;
+    for (var i = 0; i < whichForm.elements.length; i++) {
+        var element = whichForm.elements[i];
+        if (element.type == 'submit') continue;
+        var check = element.placeholder || element.getAttribute('placeholder');
+        if (!check) continue;
+        element.onfocus = function () {
+            var text = this.placeholder || this.getAttribute('placeholder');
+            if (this.value == text) {
+                this.className = '';
+                this.value = '';
+            }
+        }
+        element.onblur = function () {
+            if (this.value == '') {
+                this.className = 'placeholder';
+                this.value = this.placeholder || this.getAttribute('placeholder');
+            }
+        }
+        element.onblur();
+    }
+}
+
+function validateForm (whichform) {
+    for (var i = 0; i < whichform.elements.length; i++) {
+        var element = whichform.elements[i];
+        if (element.getAttribute('required') == 'required') {
+            if (!isFilled(element)) {
+            alert('Please fill in the ' + element.name + ' field.');
+            return false;
+            }
+        }
+        if (element.getAttribute('type') == 'email') {
+            if (!isEmail(element)) {
+                alert('The ' + element.name + ' field must be a valid email address.');
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function isFilled (field) {
+    return (field.value.length > 1 && field.value != field.placeholder);
+}
+
+function isEmail (field) {
+    return (field.value.indexOf('@') != -1 && field.value.indexOf('.') != -1);
+}
+
+function prepareForms () {
+    for (var i = 0; i < document.forms.length; i++) {
+        var thisform = document.forms[i];
+        resetFields(thisform);
+        thisform.onsubmit = function() {
+            if (!validateForm(this)) return false;
+            var article = document.getElementsByTagName('article')[0];
+            if (submitFormWithAjax(this, article)) return false;
+        return true;
+        }
+    }
+}
+
 addLoadEvent(highlightPage);
 addLoadEvent(prepareSlideshow);
 addLoadEvent(prepareInternalnav);
 addLoadEvent(preparePlaceholder);
 addLoadEvent(prepareGallery);
+addLoadEvent(focusLabels);
+addLoadEvent(prepareForms);
