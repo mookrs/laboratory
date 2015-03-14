72a01dea80cf18a0104ce7c1d297709e02a0dbde
mookrs <mookrs@gmail.com>
2015-03-14 19:28:45 +0800

    Add Ajax POST, but Chrome didn't support it as cross origin requests problem

--- 7/JaySkript/scripts/global.js
+++ 7/JaySkript/scripts/global.js
@@ -366,6 +366,76 @@ function prepareForms() {
     }
 }
 
+// Ajax
+
+function getHTTPObject() {
+    if (typeof XMLHttpRequest == 'undefined')
+        XMLHttpRequest = function() {
+            try {
+                return new ActiveXObject('Msxml2.XMLHTTP.6.0');
+            } catch (e) {}
+            try {
+                return new ActiveXObject('Msxml2.XMLHTTP.3.0');
+            } catch (e) {}
+            try {
+                return new ActiveXObject('Msxml2.XMLHTTP');
+            } catch (e) {}
+            return false;
+        }
+    return new XMLHttpRequest();
+}
+
+function displayAjaxLoading(element) {
+    while (element.hasChildNodes()) {
+        element.removeChild(element.lastChild);
+    }
+    var content = document.createElement('img');
+    content.setAttribute('src', 'images/loading.gif');
+    content.setAttribute('alt', 'Loading...');
+    element.appendChild(content);
+}
+
+function submitFormWithAjax(whichform, thetarget) {
+
+    var request = getHTTPObject();
+    if (!request) {
+        return false;
+    }
+
+    // Display a loading message.
+    displayAjaxLoading(thetarget);
+
+    // Collect the data.
+    var dataParts = [];
+    var element;
+    for (var i = 0; i < whichform.elements.length; i++) {
+        element = whichform.elements[i];
+        dataParts[i] = element.name + '=' + encodeURIComponent(element.value);
+    }
+    var data = dataParts.join('&');
+
+    request.open('POST', whichform.getAttribute('action'), true);
+    request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    request.onreadystatechange = function() {
+        if (request.readyState == 4) {
+            if (request.status == 200 || request.status == 0) {
+                var matches = request.responseText.match(/<article>([\s\S]+)<\/article>/);
+                if (matches.length > 0) {
+                    thetarget.innerHTML = matches[1];
+                } else {
+                    thetarget.innerHTML = '<p>Oops, there was an error. Sorry.</p>';
+                }
+            } else {
+                thetarget.innerHTML = '<p>' + request.statusText + '</p>';
+            }
+        }
+    };
+
+    request.send(data);
+    return true;
+}
+
 addLoadEvent(highlightPage);
 addLoadEvent(prepareSlideshow);
 addLoadEvent(prepareInternalnav);
