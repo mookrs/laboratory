180efcd680baf045fb7270f86cb9ec3833d9169b
mookrs <mookrs@gmail.com>
2015-03-15 23:39:00 +0800

    Clean Flask Code

--- 8/fanfou.py
+++ 8/fanfou.py
@@ -1,7 +1,9 @@
-from flask import Flask, session, redirect, url_for, escape, request, render_template, flash
+from flask import Flask, session, redirect, url_for, request, render_template, flash
 from flask_oauthlib.client import OAuth, OAuthException
 
 app = Flask(__name__)
+app.debug = True
+app.secret_key = 'AA0Zr98j/3yXR~XHH!jmN]LWX/,?RT'
 
 oauth = OAuth(app)
 
@@ -15,25 +17,21 @@ fanfou = oauth.remote_app(
     consumer_secret='f975fa68779936ce967921356a315853',
 )
 
-x = '2'
 
 @fanfou.tokengetter
-def get_fanfou_token(token=None):
+def get_fanfou_token():
     return session.get('fanfou_token')
 
 
 @app.route('/')
 def index():
-    session['foo'] = 42
-    global x
     if 'fanfou_token' not in session:
-        flash('Unable to load fanfou_oauth.' + x)
+        flash('Unable to load fanfou_oauth.')
     return render_template('index.html')
 
 
 @app.route('/tweet', methods=['POST'])
 def tweet():
-    session['fo'] = 42
     status = request.form['tweet']
     if not status:
         return redirect(url_for('index'))
@@ -41,23 +39,17 @@ def tweet():
         'status': status
     })
     if resp.status == 403:
-        return 'Your tweet was too long.'
+        flash('Your tweet was too long.')
     elif resp.status == 401:
-        return 'Authorization error with Twitter.'
+        flash('Authorization error with Fanfou.')
     else:
-        return '?'
+        flash('Successfully!')
     return redirect(url_for('index'))
 
 
 @app.route('/login')
 def login():
-    global x
-    x = '22'
-    callback = url_for(
-        'authorized',
-        next=request.args.get('next') or request.referrer or None,
-        _external=True
-    )
+    callback = url_for('oauthorized', next=request.args.get('next') or request.referrer or None, _external=True)
     return fanfou.authorize(callback=callback)
 
 
@@ -67,24 +59,17 @@ def logout():
     return redirect(url_for('index'))
 
 
-@app.route('/authorized')
-def authorized():
-    global x
-    x = '3'
+@app.route('/oauthorized')
+def oauthorized():
     next_url = request.args.get('next') or url_for('index')
     resp = fanfou.authorized_response()
-    if isinstance(resp, OAuthException):
-        return 'Access denied: %s' % resp.message
     if resp is None:
-        x = '4'
-        flash('Access denied')
-    else:
-        x = '5'
-        #session['fanfou_token'] = (resp['oauth_token'], '')
-        #session['fanfou_key'] = resp['oauth_consumer_key']
+        flash('You denied the request to sign in.')
+    if isinstance(resp, OAuthException):
+        flash('Access denied: %s' % resp.message)
+    session['fanfou_token'] = resp
     return redirect(next_url)
 
 
 if __name__ == '__main__':
-    app.secret_key = 'AA0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
-    app.run(debug=True)
+    app.run()
