d86c6b128e1f8750bd6c38d95c075d596b7eb56e
mookrs <mookrs@gmail.com>
2015-03-18 23:28:46 +0800

    Beautiful pythonic code

--- 10/sum_except_5_and_7.py
+++ 10/sum_except_5_and_7.py
@@ -2,8 +2,4 @@
 # -*- coding: utf-8 -*-
 
 num = raw_input("Please input a number: ")
-sum = 0
-for i in xrange(1, int(num)):
-    if not (i % 3 and i % 5):
-        sum = sum + i
-print sum
+print sum([i for i in xrange(int(num)) if not (i % 3 and i % 5)])
