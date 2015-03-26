2ccb8a05324d11bf28c1a785ab2e6c4f775e0694
mookrs <mookrs@gmail.com>
2015-03-26 23:46:43 +0800

    Clean but not best method to generate primer numbers

--- 10/print_prime_number.py
+++ 10/print_prime_number.py
@@ -3,25 +3,13 @@
 
 import sys, math
 
-def print_prime_number(num):
-    prime_list = [2]
-    current_number = 3
-    while current_number < num:
-        is_prime = True
-        current_sqrt = math.floor(math.sqrt(current_number))
-        i = 0
-
-        while i >= len(prime_list) or prime_list[i] < current_sqrt:
-            if current_number % prime_list[i] == 0:
-                is_prime = False
-                break
-            i = i + 1
-
-        if is_prime:
-            prime_list.append(current_number)
-        current_number = current_number + 1
-    
-    print prime_list
+def is_prime(n):
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
 
 
-print_prime_number(44)
+for num in range(2, 101):
+    if is_prime(num):
+        print num,
