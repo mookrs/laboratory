747aea85690aa6d36e486190f4cbdc1de6e4de20
mookrs <mookrs@gmail.com>
2015-04-01 23:05:17 +0800

    Calculate by more pythonic way

--- 10/calculate.py
+++ 10/calculate.py
@@ -2,9 +2,4 @@
 
 import math
 
-sum = 0
-
-for k in xrange(1,1000001):
-    sum = sum + math.pow(-1, k + 1) / float((2 * k - 1))
-
-print 4 * sum
+print 4 * sum(math.pow(-1, k + 1) / float((2 * k - 1)) for k in xrange(1,1000001))
