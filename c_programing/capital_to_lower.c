1276b5103c5be96e3491c2d4e467dc6f7841e2b1
mookrs <mookrs@gmail.com>
2015-12-05 11:44:28 +0800

    Add c capital_to_lower

---
+++ c_programing/capital_to_lower.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void)
+{
+    char ch1, ch2;
+
+    scanf("%c", &ch1);
+    if (ch1 > 'A' && ch1 <= 'Z')
+    {
+        ch2 = ch1 + ('a' - 'A');
+        printf("%c\n", ch2);
+    }
+    return 0;
+}
