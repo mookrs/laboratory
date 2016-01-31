3b5ff8ec47554c0df85e7faa4523e6ee10ceaa49
mookrs <mookrs@gmail.com>
2016-01-31 23:04:49 +0800

    Define project structure

---
+++ algorithms/src/1.Fundamentals/1.1BasicProgrammingModel/BinarySearch.java
@@ -0,0 +1,36 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+
+public class BinarySearch {
+    public static int indexOf(int[] a, int key) {
+        // 数组必须是有序的
+        int lo = 0;
+        int hi = a.length - 1;
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+            if (key < a[mid])
+                hi = mid - 1;
+            else if (key > a[mid])
+                lo = mid + 1;
+            else
+                return mid;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        int[] whitelist = in.readAllInts();
+
+        Arrays.sort(whitelist);
+
+        while (!StdIn.isEmpty()) {
+            int key = StdIn.readInt();
+            if (indexOf(whitelist, key) < 0)
+                StdOut.println(key);
+        }
+    }
+}
