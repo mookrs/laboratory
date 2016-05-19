9d23c7b5648e230f35b94bb5a7ac88d2ffb6f8d4
mookrs <mookrs@gmail.com>
2016-05-19 13:22:59 +0800

    Add comments in getNames.py

--- web_scraping/Chapter2/getNames.py
+++ web_scraping/Chapter2/getNames.py
@@ -4,6 +4,11 @@ from bs4 import BeautifulSoup
 
 html = urlopen("http://www.pythonscraping.com/pages/warandpeace.html")
 bsObj = BeautifulSoup(html.read(), "html.parser")
+
+# definitions in the BeautifulSoup documentation:
+# findAll(tag, attributes, recursive, text, limit, keywords)
+# find(tag, attributes, recursive, text, keywords)
+
 # this can use keyword instead. "class" is a protected keyword in Python.
 # nameList = bsObj.findAll(class_="green")
 nameList = bsObj.findAll("span", {"class": "green"}, text="the prince")
