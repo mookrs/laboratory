6049b14c0f517d8443dbdfe8c2e2370217c92224
mookrs <mookrs@gmail.com>
2016-05-21 00:17:33 +0800

    Add comments

--- web_scraping/Chapter2/childrenSiblings.py
+++ web_scraping/Chapter2/childrenSiblings.py
@@ -5,11 +5,20 @@ from bs4 import BeautifulSoup
 html = urlopen("http://www.pythonscraping.com/pages/page3.html")
 bsObj = BeautifulSoup(html, "html.parser")
 
-# for child in bsObj.find("table", {"id": "giftList"}).children:
-#     print(child)
+# If you were to write it using the `descendants()` function instead of the
+# `children()` function, about two dozen tags would be found within the
+# table and printed, including `img` tags, `span` tags, and individual `td`
+# tags.
+for child in bsObj.find("table", {"id": "giftList"}).children:
+    print(child)
 
-# for sibling in bsObj.find("table", {"id": "giftList"}).tr.next_siblings:
-#     print(sibling)
+print("----------")
 
-print(bsObj.find(
-    "img", {"src": "../img/gifts/img1.jpg"}).parent.previous_sibling.get_text())
+# by selecting the title row and calling next_siblings, we can select all
+# the rows in the table
+for sibling in bsObj.find("table", {"id": "giftList"}).tr.next_siblings:
+    print(sibling)
+
+print("----------")
+
+print(bsObj.find("img", {"src": "../img/gifts/img1.jpg"}).parent.previous_sibling.get_text())
