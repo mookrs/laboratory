df967154ce70b103edee4fd164b9faa67b301bb6
mookrs <mookrs@gmail.com>
2016-05-22 14:01:27 +0800

    getImages using regex

--- web_scraping/Chapter2/getImages.py
+++ web_scraping/Chapter2/getImages.py
@@ -6,6 +6,6 @@ import re
 html = urlopen("http://www.pythonscraping.com/pages/page3.html")
 bsObj = BeautifulSoup(html, "html.parser")
 images = bsObj.findAll(
-    "img", {"src": re.compile("\.\.\/img\/gifts/img.*\.jpg")})
+    "img", {"src": re.compile("\.\./img/gifts/img.*\.jpg")})
 for image in images:
     print(image.attrs["src"])
