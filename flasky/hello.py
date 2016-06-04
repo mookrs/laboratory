25265bf10529aa633d011ff13a736215ebf062fc
mookrs <mookrs@gmail.com>
2016-06-04 18:54:29 +0800

    Return 400 code

--- flasky/hello.py
+++ flasky/hello.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    user_agent = request.headers.get('User-Agent')
-    return '<p>Your browser is {}</p>'.format(user_agent)
+    return '<h1>Bad Request</h1>', 400
 
 
 @app.route('/user/<name>')
