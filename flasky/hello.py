55ebe8ae0a7273dddccd7a45e6be62fa70691018
mookrs <mookrs@gmail.com>
2016-06-04 18:57:30 +0800

    Return response

--- flasky/hello.py
+++ flasky/hello.py
@@ -1,11 +1,12 @@
-from flask import Flask, request
+from flask import Flask, request, make_response
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '<h1>Bad Request</h1>', 400
-
+    response = make_response('<h1>This document carries a cookie!</h1>')
+    response.set_cookie('answer', '42')
+    return response
 
 @app.route('/user/<name>')
 def user(name):
