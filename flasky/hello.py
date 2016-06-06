d0736b114c161e3a06f8d891a7effdc154a3d8f0
mookrs <mookrs@gmail.com>
2016-06-06 21:03:29 +0800

    Send async email

--- flasky/hello.py
+++ flasky/hello.py
@@ -1,5 +1,6 @@
 import os
 from datetime import datetime
+from threading import Thread
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_script import Manager, Shell
 from flask_bootstrap import Bootstrap
@@ -59,7 +60,8 @@ def index():
             db.session.add(user)
             session['known'] = False
             if app.config['FLASKY_ADMIN']:
-                send_email(app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user=user)
+                send_email(app.config['FLASKY_ADMIN'],
+                           'New User', 'mail/new_user', user=user)
         else:
             session['known'] = True
         session['name'] = form.name.data
@@ -109,8 +111,14 @@ def send_email(to, subject, template, **kwargs):
                   sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    mail.send(msg)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
 
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
 
 if __name__ == '__main__':
     manager.run()
