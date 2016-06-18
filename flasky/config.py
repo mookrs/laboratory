1cd5ab9f28ec40ae3aa10525d2241105f49a176c
mookrs <mookrs@gmail.com>
2016-06-18 20:08:05 +0800

    Disable SQLALCHEMY_TRACK_MODIFICATIONS
    see: http://stackoverflow.com/questions/33738467/how-do-i-know-if-i-can-disable-sqlalchemy-track-modifications

--- flasky/config.py
+++ flasky/config.py
@@ -8,6 +8,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
     FLASKY_ADMIN = CFG_FLASKY_ADMIN
