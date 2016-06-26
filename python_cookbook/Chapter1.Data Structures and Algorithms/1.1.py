fcfad6ee7b4e42dea45cc2c94ca527963b7109c9
mookrs <mookrs@gmail.com>
2016-06-26 22:10:35 +0800

    Add cookbook 1.1 1.2

---
+++ python_cookbook/Chapter1.Data Structures and Algorithms/1.1.py
@@ -0,0 +1,3 @@
+data = ['ACME', 50, 91.1, (2012, 12, 21)]
+_, shares, price, _ = data
+print(shares)
---
+++ python_cookbook/Chapter1.Data Structures and Algorithms/1.2.py
@@ -0,0 +1,20 @@
+record = ('Dave', 'dave@example.com', '773-555-1212', '847-555-1212')
+name, email, *phone_numbers = record
+print(name)
+print(phone_numbers)
+
+*trailing, current = [10, 8, 7, 1, 9, 5, 10, 3]
+print(trailing)
+
+record = ('ACME', 50, 123.45, (12, 18, 2012))
+name, *_, (*_, year) = record
+print(name)
+print(year)
+
+# Recursive
+items = [1, 10, 7, 4, 5, 9]
+def sum(items):
+    head, *tail = items
+    return head + sum(tail) if tail else head
+
+print(sum(items))
