51f7e13f06378d391ff255fa314fd33d9125ceaa
mookrs <mookrs@gmail.com>
2016-06-28 17:46:49 +0800

    Crawl title and content from Wikipedia, then store in MySQL

--- web_scraping/Chapter5/testMySQL.py
+++ web_scraping/Chapter5/testMySQL.py
@@ -1,8 +1,40 @@
+import datetime
+import random
+import re
+from urllib.request import urlopen
+
+from bs4 import BeautifulSoup
 import pymysql
 
-conn = pymysql.connect(host='192.168.33.10', user='root', passwd='root', db='scraping')
+conn = pymysql.connect(host='192.168.33.10', user='root',
+                       passwd='root', db='scraping', charset='utf8')
 cur = conn.cursor()
-cur.execute("SELECT * FROM pages WHERE id=1")
-print(cur.fetchone())
-cur.close()
-conn.close()
+
+random.seed(datetime.datetime.now())
+
+
+def store(title, content):
+    cur.execute(
+        "INSERT INTO pages (title, content) VALUES (%s, %s)", (title, content))
+    cur.connection.commit()
+
+
+def getLinks(articleUrl):
+    html = urlopen("http://en.wikipedia.org" + articleUrl)
+    bsObj = BeautifulSoup(html, "html.parser")
+    title = bsObj.find("h1").get_text()
+    print(title)
+    content = bsObj.find("div", {"id": "mw-content-text"}).find("p").get_text()
+    store(title, content)
+    return bsObj.find("div", {"id": "bodyContent"}).findAll("a", href=re.compile("^(/wiki/)((?!:).)*$"))
+
+
+links = getLinks("/wiki/Kevin_Bacon")
+try:
+    while len(links) > 0:
+        newArticle = links[random.randint(0, len(links) - 1)].attrs["href"]
+        print(newArticle)
+        links = getLinks(newArticle)
+finally:
+    cur.close()
+    conn.close()
