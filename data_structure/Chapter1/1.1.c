6aaa908d1f990285c05a3f46f47bb4e9eed84ffa
Mookrs Zhang <mookrs@gmail.com>
2016-09-04 15:30:20 +0800

    Merge two loops into one

--- data_structure/Chapter1/1.1.c
+++ data_structure/Chapter1/1.1.c
@@ -6,6 +6,7 @@ void swap(int *a, int *b);
 
 int main(void) {
     int numbers[] = {8, 23, 47, 4, 35, 11, 59, 41, 1, 29};
+        // int numbers[] = {1, 2, 3, 4, 4, 5, 5, 6, 7, 8};
     int N = 10;
     int k = N / 2;
     printf("第 k (%d) 个最大者为 %d\n", k, find_k_max(numbers, N, k));
@@ -30,17 +31,19 @@ int find_k_max(int numbers[], int n, int k) {
 
         /* {47, 35, 23, 8, 4} */
         /* 11 -> 59 -> 41 -> 1 -> 29 */
-        if (current_number < sorted[k-1])
+        if (current_number < sorted[k - 1])
             continue;
         else {
             int j;
 
-            for (j = k - 1; j >= 0; --j)
+            for (j = k - 1; j >= 0; --j) {
                 if (current_number < sorted[j])
                     break;
-
-            for (int m = k - 1; m > j + 1; --m)
-                sorted[m] = sorted[m - 1];
+                /* sorted[j] 小于 current_number，则用 sorted[j - 1] 的值覆盖 sorted[j]，达到依次向后移一位的效果 */
+                /* 排除 j=0 的情况（此时 current_number 是当前最大值），sorted[0] 前面没有值可以移过来了 */
+                if (j > 0)
+                    sorted[j] = sorted[j - 1];
+            }
 
             sorted[j + 1] = current_number;
         }
