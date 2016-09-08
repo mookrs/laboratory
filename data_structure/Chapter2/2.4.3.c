e4350f54345066ac6dee3e1f305b44721e36bd23
Mookrs Zhang <mookrs@gmail.com>
2016-09-08 14:25:51 +0800

    Add divide-and-conquer

--- data_structure/Chapter2/2.4.3.c
+++ data_structure/Chapter2/2.4.3.c
@@ -3,6 +3,8 @@
 
 int MaxSubsequenceSum1(const int A[], int N);
 int MaxSubsequenceSum2(const int A[], int N);
+int Max3(int A, int B, int C);
+int MaxSubSum(const int A[], int Left, int Right);
 int MaxSubsequenceSum3(const int A[], int N);
 int MaxSubsequenceSum4(const int A[], int N);
 
@@ -48,12 +50,61 @@ int MaxSubsequenceSum2(const int A[], int N) {
     return MaxSum;
 }
 
-int MaxSubsequenceSum3(const int A[], int N) {
-    return 0;
+int Max3(int A, int B, int C) {
+    int max;
 
+    if (A > B)
+        if (A > C)
+            max = A;
+        else
+            max = C;
+    else
+        if (B > C)
+            max = B;
+        else
+            max = C;
+
+    return max;
+}
+
+int MaxSubSum(const int A[], int Left, int Right) {
+    int MaxLeftSum, MaxRightSum;
+    int MaxLeftBorderSum, MaxRightBorderSum;
+    int LeftBorderSum, RightBorderSum;
+    int Center, i;
+
+    if (Left == Right) {  /* Base Case */
+        if (A[Left] > 0)
+            return A[Left];
+        else
+            return 0;
+    }
+
+    Center = (Left + Right) / 2;
+    MaxLeftSum = MaxSubSum(A, Left, Center);
+    MaxRightSum = MaxSubSum(A, Center + 1, Right);
+
+    MaxLeftBorderSum = 0; LeftBorderSum = 0;
+    for (i = Center; i >= Left; --i) {
+        LeftBorderSum += A[i];
+        if (LeftBorderSum > MaxLeftBorderSum)
+            MaxLeftBorderSum = LeftBorderSum;
+    }
+
+    MaxRightBorderSum = 0; RightBorderSum = 0;
+    for (i = Center + 1; i <= Right; ++i) {
+        RightBorderSum += A[i];
+        if (RightBorderSum > MaxRightBorderSum)
+            MaxRightBorderSum = RightBorderSum;
+    }
+
+    return Max3(MaxLeftSum, MaxRightSum, MaxLeftBorderSum + MaxRightBorderSum);
+}
+
+int MaxSubsequenceSum3(const int A[], int N) {
+    return MaxSubSum(A, 0, N - 1);
 }
 
 int MaxSubsequenceSum4(const int A[], int N) {
     return 0;
-
 }
