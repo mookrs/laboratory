7de24813f449a6f2de5b8a7f3340d334b073e43f
Mookrs <mookrs@gmail.com>
2017-09-21 18:10:26 +0800

    Just commit

--- algorithms/src/Fundamentals/BasicProgrammingModel/Ex116.java
+++ algorithms/src/Fundamentals/BasicProgrammingModel/Ex116.java
@@ -1,7 +1,10 @@
-package Fundamentals.BasicProgrammingModel;
+import edu.princeton.cs.algs4.StdOut;
 
 /**
  * Created by mookrs on 2017/1/22.
  */
 public class Ex116 {
+    public static void main(String[] args) {
+        StdOut.print(1 + 2 + "3");
+    }
 }
