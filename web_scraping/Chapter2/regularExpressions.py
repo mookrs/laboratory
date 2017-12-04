b539b935b3900268e271331bd1da21c73bc51557
Mookrs <mookrs@gmail.com>
2017-12-04 08:39:24 +0800

    Rename files according the book

--- web_scraping/Chapter2/regularExpressions.py
+++ web_scraping/Chapter2/regularExpressions.py
@@ -9,3 +9,9 @@ images = bsObj.findAll(
     "img", {"src": re.compile("\.\./img/gifts/img.*\.jpg")})
 for image in images:
     print(image.attrs["src"])
+
+html = urlopen("http://www.pythonscraping.com/pages/page2.html")
+bsObj = BeautifulSoup(html, "html.parser")
+tags = bsObj.findAll(lambda tag: len(tag.attrs) == 2)
+for tag in tags:
+    print(tag)
